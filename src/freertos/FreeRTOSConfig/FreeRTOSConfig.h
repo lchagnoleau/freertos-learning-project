a2991fd8dcfb2546a5b887d17954e141e6fdda6e
Loïc Chagnoleau <furlings@pm.me>
2024-01-10 15:24:21 +0100

    update FreeRTOS config file for our MCU

--- src/freertos/FreeRTOSConfig/FreeRTOSConfig.h
+++ src/freertos/FreeRTOSConfig/FreeRTOSConfig.h
@@ -50,21 +50,12 @@
 #define configCPU_CLOCK_HZ			SystemCoreClock
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES		( 5 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 120 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 15 * 1024 ) )
+#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 17 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 16 )
-#define configUSE_TRACE_FACILITY	1
+#define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
-
-
-#define configUSE_MUTEXES				1
-#define configUSE_COUNTING_SEMAPHORES 	1
-#define configUSE_ALTERNATIVE_API 		0
-#define configCHECK_FOR_STACK_OVERFLOW  0
-#define configUSE_RECURSIVE_MUTEXES		1
-#define configQUEUE_REGISTRY_SIZE		0
-#define configGENERATE_RUN_TIME_STATS	0
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
@@ -90,6 +81,10 @@ priority values, 0 to 15.  This must correspond to the
 configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
 NVIC value of 255. */
 #define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
+
+#define vPortSVCHandler    SVC_Handler
+#define xPortPendSVHandler PendSV_Handler
+#define xPortSysTickHandler SysTick_Handler
 
 /*-----------------------------------------------------------
  * UART configuration.
